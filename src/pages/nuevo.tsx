b7878ba78fd9ea685f98069b9494db4738ca7f29
Matias Schroder <matischroder99@gmail.com>
2023-10-10 00:06:52 -0300

    add spinner creating new tournamet

--- src/pages/nuevo.tsx
+++ src/pages/nuevo.tsx
@@ -1,45 +1,70 @@
 // pages/NuevoTorneo.tsx
 
+import CircleSpinner from '@/components/global/Spinner';
 import { postTorneo } from '@/functions/firebase/tournaments/postTorneo';
 import { withProtected } from '@/hook/route';
 import React, { useState } from 'react';
 
+import { AiOutlineCopy } from 'react-icons/ai'
+import { toast } from 'sonner';
+
 const NuevoTorneo: React.FC = () => {
     const [nombreTorneo, setNombreTorneo] = useState('');
     const [idNuevoTorneo, setIdNuevoTorneo] = useState('');
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setNombreTorneo(e.target.value);
     };
 
     const handleCrearClick = async () => {
+        setIsLoading(true);
         try {
             const id = await postTorneo(nombreTorneo);
             setIdNuevoTorneo(id);
+            toast.success('Torneo creado con éxito');
+            setIsLoading(false);
         } catch (error) {
             if (error instanceof Error) {
                 // Handle the specific error types here
                 if (error.message === "User not found") {
+                    toast.error('Usuario no encontrado');
                     // Handle the "User not found" error
                     // Display an appropriate error message to the user
                 } else if (error.message === "You have reached the limit of 25 tournaments") {
+                    toast.error('Has alcanzado el límite de 25 torneos');
                     // Handle the "Tournament limit reached" error
                     // Display an appropriate error message to the user
                 } else {
+                    toast.error('Error al crear el torneo');
                     // Handle other errors, such as "An error occurred while creating the tournament"
                     // Display a generic error message to the user
                 }
             } else {
+                toast.error('Error al crear el torneo');
                 // Handle unexpected errors here
                 // Display a generic error message to the user
             }
+            setIsLoading(false);
         }
     };
 
+    const copyToClipboard = async () => {
+        try {
+            const textToCopy = 'Text you want to copy to clipboard';
+            await navigator.clipboard.writeText(textToCopy);
+            toast.success('Identificador copiado');
+        } catch (error) {
+            console.error('Failed to copy text: ', error);
+            toast.error('Error al copiar');
+        }
+    };
+
+
     return (
-        <div className="p-4">
+        <div className="p-4 h-full">
             <h1 className="text-2xl font-bold text-center mb-4">Crear Nuevo Torneo</h1>
-            <div className="mb-4">
+            <div className="mb-4 h-full">
                 <label className="block text-gray-700 text-sm font-bold mb-2">
                     Nombre del Torneo:
                 </label>
@@ -58,16 +83,28 @@ const NuevoTorneo: React.FC = () => {
                         Crear
                     </button>
                 </div>
-                <div >
+                {isLoading && <CircleSpinner />}
+                <div>
                     {
                         idNuevoTorneo &&
-                        <div className="pt-5">
-                            Código: {idNuevoTorneo}
+                        <div className="pt-5 block">
+                            <h2 className='text-xl'>Información del torneo</h2>
+                            <div>
+                                <strong>Nombre: </strong>{nombreTorneo}
+                            </div>
+                            <div>
+                                <div className='inline-flex'>
+                                    <strong>Código: </strong>{idNuevoTorneo}
+                                </div>
+                                <div onClick={() => copyToClipboard()} className='cursor-pointer inline-flex'>
+                                    <AiOutlineCopy className='w-5 h-full ml-5' />
+                                </div>
+                            </div>
                         </div>
                     }
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
