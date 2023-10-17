a4263952f2875eb8f67b30378b71add7f318f9e0
Matias Schroder <matischroder99@gmail.com>
2023-10-17 20:57:59 -0300

    change favicon

---
+++ src/pages/_document.tsx
@@ -0,0 +1,41 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import Favicons from "@/components/global/Favicon";
+
+const DOMAINS = [
+    "https://president-bet-nextjs.vercel.app",
+];
+
+export default function Document() {
+
+    const ogImageUrl = `${DOMAINS[0]}/argentina.png`;
+
+    return (
+        <Html className="dark" lang="en">
+            <Head>
+                <Favicons />
+                <meta
+                    name="keywords"
+                    content="elecciones, argentina, elecciones presidenciales, apuestas, prode"
+                />
+                <meta
+                    property="og:title"
+                    content="Prode Elecciones Argentina" key="title"
+                />
+                <meta
+                    property="og:description"
+                    content="Aplicación para que juegues con tus amigos. Crea un nuevo torneo, guarda tu pronóstico e invita a tus amigos."
+                />
+                <meta
+                    property="og:image"
+                    content={ogImageUrl}
+                />
+
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
+
