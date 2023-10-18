2908a22409caa458f50f071fff00009dacd3c8df
Matias Schroder <matischroder99@gmail.com>
2023-10-18 00:52:57 -0300

    update app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Favicons from "@/components/global/Favicon";
 
 const DOMAINS = [
     "https://president-bet-nextjs.vercel.app",
@@ -12,7 +11,37 @@ export default function Document() {
     return (
         <Html lang="es" className="bg-[#0f0e0e]">
             <Head>
-                <Favicons />
+                <meta name="application-name" content="Prode Elecciones" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+                <meta name="apple-mobile-web-app-title" content="Prode Elecciones Argentinas" />
+                <meta name="description" content="Prode para las Elecciones Argentinas" />
+                <meta name="format-detection" content="telephone=no" />
+                <meta name="mobile-web-app-capable" content="yes" />
+                <meta name="msapplication-TileColor" content="#000000" />
+                <meta name="msapplication-tap-highlight" content="no" />
+                <meta name="theme-color" content="#000000" />
+
+                <link rel="apple-touch-icon" href="/argentina.png" />
+                <link rel="apple-touch-icon" sizes="152x152" href="/argentina.png" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/argentina.png" />
+                <link rel="apple-touch-icon" sizes="167x167" href="argentina.png" />
+
+                <link rel="icon" type="image/png" sizes="32x32" href="argentina.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="argentina.png" />
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="mask-icon" href="argentina.png" color="transparent" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+
+                <meta name="twitter:card" content="Prode para las Elecciones Argentinas" />
+                <meta name="twitter:title" content="Prode Elecciones Argentinas" />
+                <meta name="twitter:description" content="Prode para las Elecciones Argentinas" />
+                <meta name="twitter:creator" content="@matischroder" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Prode Elecciones Argentinas" />
+                <meta property="og:description" content="Prode para las Elecciones Argentinas" />
+                <meta property="og:site_name" content="Prode Elecciones Argentinas" />
+
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
                 <meta name="mobile-web-app-capable" content="yes" />
