6cd16c80feab3145f042981b8ff93db279b439ed
Matias Schroder <matischroder99@gmail.com>
2023-10-17 23:31:33 -0300

    change document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,9 +15,7 @@ export default function Document() {
                 <Favicons />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-                <meta name='viewport' content='viewport-fit=cover, width=device-width, initial-scale=1.0' />
                 <meta name="mobile-web-app-capable" content="yes" />
-                <meta name="mobile-web-app-status-bar-style" content="black" />
 
                 <meta
                     name="keywords"
